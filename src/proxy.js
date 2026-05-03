1c60c9f692af67abb7bc23808a2a2d649a4e3eb0
najiba-ta <shahidnajiba@gmail.com>
2026-05-03 20:43:48 +0600

    added privet route

--- src/proxy.js
+++ src/proxy.js
@@ -2,18 +2,19 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
 
-export async function proxy(request) {
+export async function middleware(request) {
 
     const session = await auth.api.getSession({
-        headers:await headers()
-    })
-    if (session){
-      return NextResponse.next();
-    }
-  return NextResponse.redirect(new URL('/login', request.url))
-}
+        headers: headers()
+    });
 
+    if (session) {
+        return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL('/login', request.url));
+}
 
 export const config = {
-  matcher: ['/books/:path*','/profile']
-}
+    matcher: ['/books/:path*', '/profile']
+};
